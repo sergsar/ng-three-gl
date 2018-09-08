452c4d6a5b61cd2a1032892dbef55866cfca6ad9
sergsar <sarantsev.s@gmail.com>
2018-09-09 01:05:15 +0300

    local data url

--- src/app/cube.component.ts
+++ src/app/cube.component.ts
@@ -11,7 +11,7 @@ export class CubeComponent implements OnInit {
   ngOnInit() {
     const headers = new HttpHeaders({ ['X-Tenant-Id']: 'ng_three' });
     const options = {headers: headers};
-    const url = 'https://presentation.idvp.net/cube/file/cube.json';
+    const url = 'assets/data/cube/cube.json';
     this.dataProviderService.getObservable(url, options).subscribe(p => this.data = p, () => this.data = {});
   }
 
